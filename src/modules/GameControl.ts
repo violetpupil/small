39df5b1a74002550d46b252ef6e9cdde5cb029a4
Shouguang Zhang <495140158@qq.com>
2021-08-08 20:23:35 +0800

    keydownHandler

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -6,11 +6,22 @@ class GameControl {
     snake: Snake
     food: Food
     scorePanel: ScorePanel
+    direction: string
 
     constructor() {
         this.snake = new Snake()
         this.food = new Food()
         this.scorePanel = new ScorePanel()
+        this.direction = ''
+        this.init()
+    }
+
+    init() {
+        document.addEventListener('keydown', this.keydownHandler.bind(this))
+    }
+
+    keydownHandler(event: KeyboardEvent) {
+        this.direction = event.key
     }
 }
 
