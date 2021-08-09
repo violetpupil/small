f33e652c1f91242e9ee8c6f59c509b59d8a9b9f1
Shouguang Zhang <495140158@qq.com>
2021-08-09 22:23:32 +0800

    run

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -6,22 +6,49 @@ class GameControl {
     snake: Snake
     food: Food
     scorePanel: ScorePanel
-    direction: string
+    direction = ''
+    isLive = true
 
     constructor() {
         this.snake = new Snake()
         this.food = new Food()
         this.scorePanel = new ScorePanel()
-        this.direction = ''
         this.init()
     }
 
     init() {
         document.addEventListener('keydown', this.keydownHandler.bind(this))
+        this.run()
     }
 
     keydownHandler(event: KeyboardEvent) {
         this.direction = event.key
+    }
+
+    run() {
+        let X = this.snake.X
+        let Y = this.snake.Y
+        switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+                Y -= 10
+                break
+            case "ArrowDown":
+            case "Down":
+                Y += 10
+                break
+            case "ArrowLeft":
+            case "Left":
+                X -= 10
+                break
+            case "ArrowRight":
+            case "Right":
+                X += 10
+                break
+        }
+        this.snake.X = X
+        this.snake.Y = Y
+        this.isLive && setTimeout(this.run.bind(this), 330 - this.scorePanel.level * 30)
     }
 }
 
