af558d48c76b3304cec8184b0a48cf7758830a03
Shouguang Zhang <495140158@qq.com>
2021-08-09 23:40:46 +0800

    moveBody

--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -24,7 +24,16 @@ class Snake {
         if (value < 0 || value > 290) {
             throw new Error("GAME OVER!")
         }
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            if (value > this.X) {
+                value = this.X - 10
+            } else {
+                value = this.X + 10
+            }
+        }
+        this.moveBody()
         this.head.style.left = value + 'px'
+        this.checkHeadBody()
     }
 
     set Y(value) {
@@ -34,11 +43,38 @@ class Snake {
         if (value < 0 || value > 290) {
             throw new Error("GAME OVER!")
         }
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            if (value > this.Y) {
+                value = this.Y - 10
+            } else {
+                value = this.Y + 10
+            }
+        }
+        this.moveBody()
         this.head.style.top = value + 'px'
+        this.checkHeadBody()
     }
 
     addBody() {
         this.element.insertAdjacentHTML("beforeend", "<div></div>")
+    }
+
+    moveBody() {
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+            (this.bodies[i] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+        }
+    }
+
+    checkHeadBody() {
+        for (let i = 1; i < this.bodies.length; i++) {
+            let bd = this.bodies[i] as HTMLElement
+            if (this.X === bd.offsetLeft && this.Y === bd.offsetTop) {
+                throw new Error("GAME OVER!")
+            }
+        }
     }
 }
 
