53d43e1c40f8db987bfcd87b7affd04aff037c65
Shouguang Zhang <495140158@qq.com>
2021-08-14 02:02:08 +0800

    KEYDOWN

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(screen)
     while True:
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
 
 
--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,16 @@ import sys
 
 import pygame
 
+from ship import Ship
 
-def check_events():
+
+def check_events(ship: Ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx += 1
 
 
 def update_screen(ai_settings, screen, ship):
