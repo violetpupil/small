46b8589ef7284c84cd6361ff8934494332785fd9
Shouguang Zhang <495140158@qq.com>
2021-08-14 02:10:30 +0800

    update

--- game_functions.py
+++ game_functions.py
@@ -11,7 +11,10 @@ def check_events(ship: Ship):
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                ship.rect.centerx += 1
+                ship.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
 
 
 def update_screen(ai_settings, screen, ship):
