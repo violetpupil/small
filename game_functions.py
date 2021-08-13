b98dfeef63fb2a235569895c467c9701cf6e8e61
Shouguang Zhang <495140158@qq.com>
2021-08-14 01:55:02 +0800

    update_screen

--- game_functions.py
+++ game_functions.py
@@ -7,3 +7,9 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+
+def update_screen(ai_settings, screen, ship):
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
