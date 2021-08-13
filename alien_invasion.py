4f9450cd60fef69915711770c4aaac89e3255f81
Shouguang Zhang <495140158@qq.com>
2021-08-14 01:16:32 +0800

    bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,12 +5,15 @@ import pygame
 
 def run_game():
     pygame.init()
-    pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
         pygame.display.flip()
 
 
