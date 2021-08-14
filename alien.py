82f5b2fd168dc31e971937cddc208d34d6b81c9f
Shouguang Zhang <495140158@qq.com>
2021-08-14 15:07:29 +0800

    update_aliens

--- alien.py
+++ alien.py
@@ -1,9 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
 
+from settings import Settings
+
 
 class Alien(Sprite):
-    def __init__(self, ai_settings, screen):
+    def __init__(self, ai_settings: Settings, screen):
         super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
@@ -15,3 +17,7 @@ class Alien(Sprite):
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        self.x += self.ai_settings.alien_speed_factor
+        self.rect.x = self.x
