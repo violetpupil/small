760ce41aa5137ff0dab2a767a791102736516ebd
Shouguang Zhang <495140158@qq.com>
2021-08-14 23:34:25 +0800

    check_play_button

--- game_functions.py
+++ game_functions.py
@@ -34,7 +34,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship: Ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship: Ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -42,6 +42,14 @@ def check_events(ai_settings, screen, ship: Ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def update_screen(ai_settings, screen, stats, ship: Ship, aliens, bullets, play_button):
