c8aaceb0ffa54f2700dbc28062a27119d8fd80ba
Shouguang Zhang <495140158@qq.com>
2021-08-14 09:57:54 +0800

    ship_speed_factor

--- ship.py
+++ ship.py
@@ -1,22 +1,27 @@
 import pygame
 
+from settings import Settings
+
 
 class Ship:
-    def __init__(self, screen: pygame.Surface):
+    def __init__(self, ai_settings: Settings, screen: pygame.Surface):
         self.screen = screen
+        self.ai_settings = ai_settings
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
