a0f862e37142d7deba9dd104f20cd1a3f7f37ab6
Shouguang Zhang <495140158@qq.com>
2021-08-15 12:54:26 +0800

    check_high_score

--- game_functions.py
+++ game_functions.py
@@ -89,10 +89,18 @@ def check_bullet_alien_collisions(ai_settings: Settings, screen, stats: GameStat
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
+
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
+
+
+def check_high_score(stats: GameStats, sb: Scoreboard):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
 
 
 def get_number_aliens_x(ai_settings, alien_width):
