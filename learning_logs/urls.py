4575fe3e7b0f0c47e912f177bfb8ecdcc3857be2
Shouguang Zhang <495140158@qq.com>
2021-10-17 14:40:09 +0800

    new_topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("topics", views.topics, name="topics"),
     re_path(r"^topics/(?P<topic_id>\d+)$", views.topic, name="topic"),
+    path("new_topic", views.new_topic, name="new_topic"),
 ]
