4455acdb472bc72287ca28373086d89876903ea3
Shouguang Zhang <495140158@qq.com>
2021-10-17 21:05:56 +0800

    new_entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("topics", views.topics, name="topics"),
     re_path(r"^topics/(?P<topic_id>\d+)$", views.topic, name="topic"),
     path("new_topic", views.new_topic, name="new_topic"),
+    re_path(r"^new_entry/(?P<topic_id>\d+)$", views.new_entry, name='new_entry'),
 ]
