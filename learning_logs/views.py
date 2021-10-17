317f07a25ac1f2a7699594c5eaaee7675b11ee06
Shouguang Zhang <495140158@qq.com>
2021-10-17 10:34:04 +0800

    topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,3 +10,10 @@ def index(request):
 def topics(request):
     context = {'topics': Topic.objects.order_by('date_added')}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    t = Topic.objects.get(id=topic_id)
+    entries = t.entry_set.order_by('-date_added')
+    context = {'topic': t, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
