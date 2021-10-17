e0b08b2d3b781c384a2e6ac58eb2164392a2eb82
Shouguang Zhang <495140158@qq.com>
2021-10-17 09:45:24 +0800

    topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
 
 def index(request):
     return render(request, "learning_logs/index.html")
+
+
+def topics(request):
+    context = {'topics': Topic.objects.order_by('date_added')}
+    return render(request, 'learning_logs/topics.html', context)
