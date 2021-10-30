106c29c6d8f359ec51ca9a6a5225a4ca855652c1
Shouguang Zhang <495140158@qq.com>
2021-10-30 08:31:52 +0800

    app_name

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from django.urls import path
 
-app_name = "urls"
+app_name = "users"
 urlpatterns = [
     path("login", login, {"template_name": "users/login.html"}, name="login"),
 ]
