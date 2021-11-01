9af62701e88a3473cc0c9082d09c0f3ccec2c000
Shouguang Zhang <495140158@qq.com>
2021-11-01 23:01:17 +0800

    logout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
+from users import views
+
 app_name = "users"
 urlpatterns = [
     path("login", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
