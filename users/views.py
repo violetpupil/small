479b4b71edd08847cdba2b4e0b707ad0b294c02f
Shouguang Zhang <495140158@qq.com>
2021-11-02 22:12:46 +0800

    register

--- users/views.py
+++ users/views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, redirect
 
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('learning_logs:index'))
+    return redirect('learning_logs:index')
 
 
 def register(request):
@@ -17,8 +15,7 @@ def register(request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
-            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
-            login(request, authenticated_user)
-            return HttpResponseRedirect(reverse('learning_logs:index'))
+            login(request, authenticate(username=new_user.username, password=new_user.password))
+            return redirect('learning_logs:index')
 
     return render(request, 'users/register.html', {'form': form})
