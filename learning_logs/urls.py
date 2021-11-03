02817ceea573c00138236008246370640ee82348
Shouguang Zhang <495140158@qq.com>
2021-11-03 14:12:15 +0800

    path

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from learning_logs import views
 
@@ -6,8 +6,8 @@ app_name = "learning_logs"
 urlpatterns = [
     path('', views.index, name='index'),
     path("topics", views.topics, name="topics"),
-    re_path(r"^topics/(?P<topic_id>\d+)$", views.topic, name="topic"),
+    path("topics/<int:topic_id>", views.topic, name="topic"),
     path("new_topic", views.new_topic, name="new_topic"),
-    re_path(r"^new_entry/(?P<topic_id>\d+)$", views.new_entry, name='new_entry'),
-    re_path(r'^edit_entry/(?P<entry_id>\d+)$', views.edit_entry, name='edit_entry'),
+    path("new_entry/<int:topic_id>", views.new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
