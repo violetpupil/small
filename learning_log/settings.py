86e8d40db76f7dfc90c79786f6e33486da58eda3
Shouguang Zhang <495140158@qq.com>
2021-11-05 10:39:04 +0800

    clear

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -81,9 +81,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
