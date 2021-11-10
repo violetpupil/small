b543846c9c5713c3f2d732d0dead0a8c4b14e264
Shouguang Zhang <495140158@qq.com>
2021-11-10 18:58:57 +0800

    clear

--- util/django.py
+++ util/django.py
@@ -5,4 +5,3 @@ def production_settings(settings):
     settings["DEBUG"] = False
     settings["ALLOWED_HOSTS"] = ['*']
     settings["DATABASES"] = {'default': dj_database_url.config(default="postgres://localhost")}
-    settings["SECURE_PROXY_SSL_HEADER"] = ('HTTP_X_FORWARDED_PROTO', 'https')
