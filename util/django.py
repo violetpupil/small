06d50456e0dba3967905db3a650ce9c8945a856e
Shouguang Zhang <495140158@qq.com>
2021-11-10 16:35:14 +0800

    production_settings

--- util/django.py
+++ util/django.py
@@ -3,5 +3,7 @@ import dj_database_url
 
 def production_settings(settings):
     settings["DEBUG"] = False
-    settings["DATABASES"] = {'default': dj_database_url.config(default="postgres://localhost")}
     settings["ALLOWED_HOSTS"] = ['*']
+    settings["DATABASES"] = {'default': dj_database_url.config(default="postgres://localhost")}
+    settings["SECURE_PROXY_SSL_HEADER"] = ('HTTP_X_FORWARDED_PROTO', 'https')
+    settings["STATIC_ROOT"] = "staticfiles"
