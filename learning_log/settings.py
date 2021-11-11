6faca03bc713f3fa6e8ba24c0279383e9db7a6ec
Shouguang Zhang <495140158@qq.com>
2021-11-11 16:30:11 +0800

    TEMPLATES

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "learning_log" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
