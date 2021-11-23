723d2884f6ba8a79f9e4022457f05015bc25995e
Shouguang Zhang <495140158@qq.com>
2021-11-23 16:19:02 +0800

    Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default class Footer extends React.Component {
   render() {
+    const { todoList } = this.props
+    const doneCount = todoList.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0)
+    const total = todoList.length
     return (
       <div>
         <input />
-        <span></span>
+        <span>{doneCount}/{total}</span>
         <button>clear done</button>
       </div>
     )
