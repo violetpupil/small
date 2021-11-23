b30a621dd798b018e0271347a17cb48188be70be
Shouguang Zhang <495140158@qq.com>
2021-11-23 19:21:14 +0800

    handleKeyUp

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 export default class Header extends React.Component {
+  handleKeyUp = event => {
+    const { key, target } = event
+    if (key !== "Enter") return
+    if (target.value.trim() === '') {
+      alert("content cannot be empty")
+      return
+    }
+    this.props.addTodo({ name: target.value, done: false })
+    target.value = ''
+  }
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="enter content and press Enter to add" />
+        <input onKeyUp={this.handleKeyUp} type="text" placeholder="enter content and press Enter to add" />
       </div>
     )
   }
