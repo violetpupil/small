095fa32c6849d62c7d56230fc1571cc5303b0c3e
Shouguang Zhang <495140158@qq.com>
2021-11-23 17:02:46 +0800

    center

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import List from './components/List'
 import Footer from './components/Footer'
+import "./App.css"
 
 export default class App extends React.Component {
   state = {
@@ -11,11 +12,11 @@ export default class App extends React.Component {
   render() {
     const { todoList } = this.state
     return (
-      <>
+      <div className="todo-container">
         <Header />
         <List todoList={todoList} />
         <Footer todoList={todoList} />
-      </>
+      </div>
     )
   }
 }
