88f8dbb106908421d67057ef8445c16517f03c92
Shouguang Zhang <495140158@qq.com>
2021-11-23 18:44:59 +0800

    addTodo

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import Footer from './components/Footer'
 import "./App.css"
 
 export default class App extends React.Component {
-  state = {
-    todoList: []
-  }
+  state = { todoList: [] }
+
+  addTodo = todo => this.setState({ todoList: [todo, ...this.state.todoList] })
 
   render() {
     const { todoList } = this.state
     return (
       <div className="todo-container">
-        <Header />
+        <Header addTodo={this.addTodo} />
         <List todoList={todoList} />
         <Footer todoList={todoList} />
       </div>
