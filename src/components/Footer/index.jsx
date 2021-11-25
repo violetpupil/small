54345409091799462cbea5f8785a60f2ea5ca6d4
Shouguang Zhang <495140158@qq.com>
2021-11-25 17:09:19 +0800

    handleCheckAllTodo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 export default class Footer extends React.Component {
+  handleCheckAllTodo = event => this.props.checkAllTodo(event.target.checked)
+
+  handleClearAllDone = () => this.props.clearAllDone()
+
   render() {
     const { todoList } = this.props
     const doneCount = todoList.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0)
     const total = todoList.length
     return (
       <div>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          onChange={this.handleCheckAllTodo}
+          checked={doneCount === total && total !== 0}
+        />
         <span>{doneCount}/{total}</span>
-        <button>clear done</button>
+        <button onClick={this.handleClearAllDone}>clear done</button>
       </div>
     )
   }
