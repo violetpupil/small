ff19342e33755a73af7cd7bce9b5bf993558f670
Shouguang Zhang <495140158@qq.com>
2021-11-25 16:09:12 +0800

    propTypes

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Item from '../Item'
 import "./index.css"
 
 export default class List extends React.Component {
+  static propTypes = {
+    todoList: PropTypes.array.isRequired,
+    updateTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired,
+  }
+
   render() {
     const { todoList, updateTodo, deleteTodo } = this.props
     return (
