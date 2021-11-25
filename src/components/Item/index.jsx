3ddd00dfd504f8ad8a3dfad3bc84f71ecf127a02
Shouguang Zhang <495140158@qq.com>
2021-11-25 15:23:26 +0800

    mouse

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import "./index.css"
 
 export default class Item extends React.Component {
+  state = { mouse: false }
+
+  handleMouse = flag => () => this.setState({ mouse: flag })
+
   render() {
     const { name } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li
+        style={{ backgroundColor: mouse ? '#ddd' : 'white' }}
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}
+      >
         <label>
           <input type="checkbox" />
           <span>{name}</span>
         </label>
-        <button>delete</button>
+        <button style={{ display: mouse ? 'block' : 'none' }}>delete</button>
       </li>
     )
   }
